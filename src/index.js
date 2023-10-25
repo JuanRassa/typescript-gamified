897593123d55f20a5cb2e209b013838578c147e1
Juan Rassa <jmrassa.dev@gmail.com>
2023-10-25 01:13:03 +0100

    feat: Create a color picker selector.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 "use strict";
 const pixels = [];
+const unactivePixelColor = '#efefe1';
+const arrayOfColors = ['black', 'gold', 'orange', 'royalblue', 'red', 'violet', 'green', 'blue', 'yellow', 'purple'];
+const colorSelector = document.querySelector('#color-picker');
+const colorDisplay = document.querySelector('#color-display');
+colorDisplay.style.backgroundColor = arrayOfColors[0];
+colorSelector.addEventListener('input', () => {
+    colorDisplay.style.backgroundColor = colorSelector.value;
+});
 function createPixel(pixel) {
     const pixelBtn = document.createElement('button');
     pixelBtn.setAttribute('data-x-coordinate', pixel.x.toString());
     pixelBtn.setAttribute('data-y-coordinate', pixel.y.toString());
     pixelBtn.setAttribute('data-active', pixel.active.toString());
+    pixelBtn.setAttribute('data-color', pixel.color);
     pixelBtn.classList.add('pixelBtn');
     pixelBtn.id = `${pixel.x}-${pixel.y}`;
     pixelBtn.onclick = function () {
@@ -13,31 +22,45 @@ function createPixel(pixel) {
     };
     return pixelBtn;
 }
+function createColorOption() {
+    arrayOfColors.forEach(color => {
+        const colorPickerOption = document.createElement('option');
+        colorPickerOption.setAttribute('value', color);
+        colorPickerOption.innerHTML = color;
+        colorSelector.append(colorPickerOption);
+    });
+}
 function updateState(x, y) {
     const pixelToUpdate = pixels.find(p => p.x == x && p.y == y);
-    if (pixelToUpdate)
+    if (pixelToUpdate) {
         pixelToUpdate.active = !pixelToUpdate.active;
+        if (pixelToUpdate.active) {
+            pixelToUpdate.color = colorSelector.value;
+        }
+    }
 }
 function createCanvas() {
-    const canvasSize = 8;
-    // const canvasColumns = 8;
-    // const canvasRows = 8;
+    const canvasSize = 23;
     const canvas = document.getElementById('canvas');
     for (let x = 0; x < canvasSize; x++) {
         for (let y = 0; y < canvasSize; y++) {
-            pixels.push({ x: x, y: y, active: false });
-            const pixelBtn = createPixel({ x: x, y: y, active: false });
+            pixels.push({ x: x, y: y, active: false, color: '#e1e1e1' });
+            const pixelBtn = createPixel({
+                x: x,
+                y: y,
+                active: false,
+                color: '#e1e1e1',
+            });
             canvas === null || canvas === void 0 ? void 0 : canvas.append(pixelBtn);
         }
     }
-    // console.table(pixels);
+    createColorOption();
 }
 function refreshCanvas() {
     for (const p of pixels) {
         const pixelBtn = document.getElementById(`${p.x}-${p.y}`);
         if (pixelBtn) {
-            console.log(typeof pixelBtn);
-            pixelBtn.style.backgroundColor = p.active ? '#28bb84' : '#efefe1';
+            pixelBtn.style.backgroundColor = p.active ? p.color : unactivePixelColor;
         }
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,21 @@
-const pixels: { x: number; y: number; active: boolean }[] = [];
+const pixels: { x: number; y: number; active: boolean; color: string }[] = [];
+const unactivePixelColor = '#efefe1';
+const arrayOfColors: string[] = ['black', 'gold', 'orange', 'royalblue', 'red', 'violet', 'green', 'blue', 'yellow', 'purple'];
 
-function createPixel(pixel: {
-  x: number;
-  y: number;
-  active: boolean;
-}): HTMLButtonElement {
+const colorSelector: HTMLSelectElement = document.querySelector('#color-picker') as HTMLSelectElement;
+const colorDisplay: HTMLElement = document.querySelector('#color-display') as HTMLElement;
+colorDisplay.style.backgroundColor = arrayOfColors[0];
+
+colorSelector.addEventListener('input', () => {
+  colorDisplay.style.backgroundColor = colorSelector.value;
+});
+
+function createPixel(pixel: { x: number; y: number; active: boolean; color: string }): HTMLButtonElement {
   const pixelBtn = document.createElement('button');
   pixelBtn.setAttribute('data-x-coordinate', pixel.x.toString());
   pixelBtn.setAttribute('data-y-coordinate', pixel.y.toString());
   pixelBtn.setAttribute('data-active', pixel.active.toString());
+  pixelBtn.setAttribute('data-color', pixel.color);
   pixelBtn.classList.add('pixelBtn');
   pixelBtn.id = `${pixel.x}-${pixel.y}`;
 
@@ -19,35 +26,50 @@ function createPixel(pixel: {
 
   return pixelBtn;
 }
+function createColorOption(): void {
+  arrayOfColors.forEach(color => {
+    const colorPickerOption = document.createElement('option');
+    colorPickerOption.setAttribute('value', color);
+    colorPickerOption.innerHTML = color;
+    colorSelector.append(colorPickerOption);
+  });
+}
 
 function updateState(x: number, y: number): void {
   const pixelToUpdate = pixels.find(p => p.x == x && p.y == y);
-  if (pixelToUpdate) pixelToUpdate.active = !pixelToUpdate.active;
+  if (pixelToUpdate) {
+    pixelToUpdate.active = !pixelToUpdate.active;
+    if (pixelToUpdate.active) {
+      pixelToUpdate.color = colorSelector.value;
+    }
+  }
 }
 
 function createCanvas(): void {
-  const canvasSize = 8;
-  // const canvasColumns = 8;
-  // const canvasRows = 8;
+  const canvasSize = 23;
   const canvas = document.getElementById('canvas');
 
   for (let x = 0; x < canvasSize; x++) {
     for (let y = 0; y < canvasSize; y++) {
-      pixels.push({ x: x, y: y, active: false });
+      pixels.push({ x: x, y: y, active: false, color: '#e1e1e1' });
 
-      const pixelBtn = createPixel({ x: x, y: y, active: false });
+      const pixelBtn = createPixel({
+        x: x,
+        y: y,
+        active: false,
+        color: '#e1e1e1',
+      });
       canvas?.append(pixelBtn);
     }
   }
-  // console.table(pixels);
+  createColorOption();
 }
 
 function refreshCanvas(): void {
   for (const p of pixels) {
     const pixelBtn = document.getElementById(`${p.x}-${p.y}`);
     if (pixelBtn) {
-      console.log(typeof pixelBtn);
-      pixelBtn.style.backgroundColor = p.active ? '#28bb84' : '#efefe1';
+      pixelBtn.style.backgroundColor = p.active ? p.color : unactivePixelColor;
     }
   }
 }
