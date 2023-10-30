827eae6012fd13a127998ea3d23d8c0a28b380c9
Juan Rassa <jmrassa.dev@gmail.com>
2023-10-30 08:46:34 +0000

    feat(init): :tada: Setup TS and Node.

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import * as readlineSync from 'readline-sync'; // Don't worry about this
+// We'll cover `import` in future levels.
+const answer = readlineSync.question('Hello player, welcome!');
+console.log(answer);
